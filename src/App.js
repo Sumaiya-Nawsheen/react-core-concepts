0f29fbf631447eb1b6c39a566ba442f5004825e0
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-02-25 10:36:02 +0600

    react-conponent

--- src/App.js
+++ src/App.js
@@ -2,24 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const products =[
+        {name:'Illustrator', price:'$60.99'},
+        {name:'PhotoShop', price:'$90.99'},
+        {name:'PDF Reader', price:'$6.99'}
+    ]
+    return ( <div className = "App" >
+        <header className = "App-header" >
+        <img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <p >
+        Edit < code > src / App.js </code> and save to reload. </p>
+        <Product product={products[0]} ></Product>
+        <Product product={products[1]} ></Product>
+        <Product product={products[2]} ></Product>
+        
+        < Person > </Person>  
+        <Person > </Person> 
+        <a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        My first react paragraph. </a> 
+       </header> 
+          </div>
+    );
 }
+
+function Product(props){
+    const productStyle = {
+        border: "1px solid gray",
+        borderRadius: '5px',
+        backgroundColor: 'lightgray',
+        height:'250px',
+        width:'200px',
+        float:'left',
+        color:'blue'
+    }
+    return(
+        <div style={productStyle}>
+       <h3>{props.product.name} </h3>
+       <h1>{props.product.price}</h1>
+       <button>Buy Now</button>
+        </div>
+    )
+}
+
+function Person() {
+    const personStyle = {
+        border: '2px solid red',
+        margin: '10px'
+    }
+    return ( <div>
+        <h3 > Hi </h3> 
+        </div >
+    )
+}
+
+
 
 export default App;
